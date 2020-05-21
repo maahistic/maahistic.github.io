7d526c2d8c138836a1ec91e2d2afd87a023d2d3e
Mahi <51745455+maahistic@users.noreply.github.com>
2020-05-21 13:39:19 +0530

    Copyright Reserved for Mahendranath
    Copyright Reserved for Mahendranath

---
+++ js/main.js
@@ -0,0 +1,240 @@
+var projectArray=[
+    {
+        name: "Online Pet Plus ",
+        disp: "An Online Web App to use for Pet Hospital Management.",
+        language: "PHP, JQuery, JavaScript, CSS, HTML5, Bootstrap",
+        purpose: "College Project",
+ //     link: "https://play.google.com/store/apps/details?id=com.bdf.doc.testrreminderui&hl=en",
+        pImage: "bg.jpg"
+    },
+/*    {
+        name: "EDcrypt",
+        disp: "Password Encryption and Decryption Android App to Store Password Securly anyware.",
+        language: "JAVA, XML, Android",
+        purpose: "Self Learning",
+        link: "https://github.com/XenonAman/EDcrypt",
+        pImage: "bg.jpg"
+    },*/
+    {
+        name: "Serice Disruption",
+        disp: "Web Application for Apply the service Disruption for FedEx.",
+        language: "HTML, CSS, Java, SpringBoot, SQL Developer",
+        purpose: "Project Requirement",
+        link: "https://github.com/XenonAman/CalculatorSaud",
+        pImage: "bg.jpg"
+    },
+ /*   {
+        name: "E-Cell StudStart",
+        disp: "Web portal for E-Cell VSIT, Startup Jobs site for Freshers Studesnts.",
+        language: "MERN Stack",
+        purpose: "Third Year Project",
+        link: "https://github.com/ashutoshtrack/Proj-SAS",
+        pImage: "bg.jpg"
+    },
+    {
+        name: "ToDo List WebApp",
+        disp: "ToDo List WebApp with Login and Signin for maintaining the self ToDo's.",
+        language: "MERN Stack",
+        purpose: "Intership Selection",
+        link: "https://github.com/XenonAman/ToDoList_ReactWebApp",
+        pImage: "bg.jpg"
+    },
+    {
+        name: "Bizcon 2017 & 2018 Website",
+        disp: "E-Cell annual fest BIZCON  2018 and 2019 Informatic website.",
+        language: "HTML, CSS, Bootstrap, JS",
+        purpose: "E-Cell | VSIT Fest",
+        link: "http://ecell.vsit.edu.in/bizcon/",
+        pImage: "bg.jpg"
+    },
+    {
+        name: "Website Templates",
+        disp: "Self Learning and getting grip on FrontEnd Side and Other Contributions.",
+        language: "HTML, CSS, Bootstrap, JS",
+        purpose: "Self Learning & Templates",
+        link: "https://github.com/XenonAman/Websites",
+        pImage: "bg.jpg"
+    },
+    {
+        name: "Excel Web Automation",
+        disp: "Web Scarping Excel Macros to reduce time and Efford in Curent Project Porcess.",
+        language: "VBA",
+        purpose: "Project Process @ Infosys ltd.",
+        link: "https://github.com/XenonAman?tab=repositories",
+        pImage: "bg.jpg"
+    },*/
+    {
+        name: "Other Contributions",
+        disp: "Self Learning, getting grip on Different Languages and Contributions on Others Projects.",
+        language: "____________",
+        purpose: "Self Learning",
+        link: "https://github.com/XenonAman?tab=repositories",
+        pImage: "bg.jpg"
+    },
+]
+
+var skillArray = [
+    {
+        skillIcon: "c++.jpg",
+        skill : "C ++",
+        level : 55
+    },
+    {
+        skillIcon: "java.jpg",
+        skill : "Java",
+        level : 65
+    },
+    {
+        skillIcon: "js.jpg",
+        skill : "JavaScript",
+        level : 70
+    },
+    {
+        skillIcon: "py.jpg",
+        skill : "Python",
+        level : 65
+    },
+    {
+        skillIcon: "php.jpg",
+        skill : "PHP",
+        level : 60
+    },
+    /*{
+        skillIcon: "springBoot.jpg",
+        skill : "SpringBoot",
+        level : 70
+    },*/
+    {
+        skillIcon: "sql.jpg",
+        skill : "MySQL",
+        level : 90
+    },
+    {
+        skillIcon: "access.jpg",
+        skill : "Microsoft Access",
+        level : 70
+    },
+    {
+        skillIcon: "html.jpg",
+        skill : "HTML 5",
+        level : 95
+    },
+    {
+        skillIcon: "css.jpg",
+        skill : "CSS 3",
+        level : 90
+    },
+    {
+        skillIcon: "bs.jpg",
+        skill : "Bootstrap",
+        level : 75
+    },
+    /*{
+        skillIcon: "wp.jpg",
+        skill : "Wordpress",
+        level : 40
+    },
+    {
+        skillIcon: "ps.jpg",
+        skill : "Adobe Photoshop CS6",
+        level : 80
+    },
+    {
+        skillIcon: "ai.jpg",
+        skill : "Adobe Illustrator",
+        level : 60	
+    },*/
+    /*{
+        skillIcon: "ae.jpg",
+        skill : "Adobe After Effects",
+        level : 65
+    },
+    {
+        skillIcon: "xd.jpg",
+        skill : "Adobe XD",
+        level : 50
+    },*/
+    {
+        skillIcon: "git.jpg",
+        skill : "GitHub",
+        level : 75
+    },
+    {
+        skillIcon: "ms.jpg",
+        skill : "Microsoft Office",
+        level : 85
+    },
+]
+
+var cCertiArray = [
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "1st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "2st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "3st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "4st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "5st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "6st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "7st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "8st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "9st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "10st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "11st Certificate"
+    },
+    {
+        cCertiImg: "dum.png",
+        cCertiName: "12st Certificate"
+    }
+]
+
+var pCertiArray = [
+    {
+        pCertiImg: "dum.png",
+        pCertiName: "1st Certificate"
+    },
+    {
+        pCertiImg: "dum.png",
+        pCertiName: "1st Certificate"
+    },
+    {
+        pCertiImg: "dum.png",
+        pCertiName: "2st Certificate"
+    },
+]
+
+        // mainArray.forEach((element,index) => {
+        //     setTimeout(function(){
+        //         document.getElementById("id1").innerHTML = "Skills: " + element.skill;
+        //         document.getElementById("id2").innerHTML = "Level: " + element.level;
+        //      }, 1000 * index);     
+        // });
